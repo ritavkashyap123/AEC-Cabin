36d146150c88de3c2459ab3561ed8a02a40f9b62
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-12-21 23:47:26 +0530

    login/signup done

--- AEC-Cabin/App.js
+++ AEC-Cabin/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/auth/Login';
+import Signup from './components/auth/SignUp';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Ritav</Text>
-      <StatusBar style="auto" />
+      {/* <Login /> */}
+      <Signup />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    height: "100%",
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ AEC-Cabin/components/auth/SignUp.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+} from "react-native";
+
+export default function Signup() {
+  const [text, onChangeText] = useState();
+  const [number, onChangeNumber] = useState();
+  const [mail, onChangeMail] = useState();
+  const [password, onChangePassword] = useState();
+  const [login, setLogin] = useState();
+
+  return (
+    <View style={styles.page}>
+      <View style={styles.box}>
+        <Text style={styles.head}>Sign up your account</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Full name"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Phone Number"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeMail}
+          value={mail}
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+          placeholder="Password"
+        />
+
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "#43CA7D" : "#11624F",
+            },
+            styles.button,
+          ]}
+          onPress={login}
+        >
+          <Text style={styles.text}>Sign up</Text>
+        </Pressable>
+        <Pressable onPress={{}}>
+          <Text style={styles.content}>
+            Already have an account? <Text style={styles.login}>Login</Text>
+          </Text>
+        </Pressable>
+        <View style={styles.terms}>
+          <Text style={{ color: "#888" }}>Terms & Conditions</Text>
+          <Text style={{ color: "#888" }}>Privacy Policy</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: "#43CA7D",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: "90%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  head: {
+    fontSize: 20,
+    margin: 20,
+  },
+  input: {
+    width: "90%",
+    margin: 10,
+    height: 40,
+    padding: 10,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  content: {
+    fontSize: 15,
+    margin: 10,
+  },
+  login: {
+    color: "blue",
+  },
+  terms: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    margin: 20,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    margin: 10,
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
+    letterSpacing: 0.3,
+    color: "white",
+  },
+});
